ec40e733f76ad022621973d9c9c2d94b54e94232
Bob Nisco <BobNisco@gmail.com>
2014-04-29 13:19:38 -0400

    Read from file function, add some sample data

--- src/Encode.java
+++ src/Encode.java
@@ -1,7 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author Bob Nisco
  */
 
 public class Encode {
 
+	public Encode() {
+		super();
+	}
+
+	/**
+	 * An internal handler for reading from a file
+	 * @param filePath the file path of the file to be read
+	 * @return a string representation of the file
+	 */
+	private String readFromFile(String filePath) {
+		// Utilize Java 7's resources feature to auto-close the file
+		// so that we don't need to use a finally block to close it
+		try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			StringBuilder stringBuilder = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				stringBuilder.append(line);
+				line = br.readLine();
+			}
+			return stringBuilder.toString();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	/**
+	 * A main method for Encode
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Encode encode = new Encode();
+		String sourceFilePath;
+		String targetFilePath;
+
+		if (args.length == 2) {
+			sourceFilePath = args[0];
+			targetFilePath = args[1];
+		} else {
+			// Sample data for ease of use when running while developing
+			sourceFilePath = "samples/input/sample1.txt";
+			targetFilePath = "samples/output/sample1.txt";
+		}
+
+		System.out.println(encode.readFromFile(sourceFilePath));
+	}
 }
