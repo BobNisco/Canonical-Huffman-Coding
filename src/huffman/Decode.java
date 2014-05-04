fcf784d0c638019d2efda4b40036d2fda045428f
Bob Nisco <BobNisco@gmail.com>
2014-05-04 19:14:44 -0400

    Stub out Decode class

--- src/huffman/Decode.java
+++ src/huffman/Decode.java
@@ -5,5 +5,41 @@ package huffman;
  * @author Bob Nisco
  */
 public class Decode {
+	public String sourceFilePath;
+	public String targetFilePath;
 
+	public Decode() {
+		super();
+	}
+
+	public Decode(String sourceFilePath, String targetFilePath) {
+		this.sourceFilePath = sourceFilePath;
+		this.targetFilePath = targetFilePath;
+	}
+
+	/**
+	 * The runner function to perform the decoding
+	 */
+	private void performDecode() {
+		// 1. Read in file and build huffman tree
+		// 2. Store codes for lookup
+		// 3. Decode data and write character output
+	}
+
+	/**
+	 * A main method for huffman.Decode
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args) {
+		Decode decode;
+
+		if (args.length == 2) {
+			decode = new Decode(args[0], args[1]);
+		} else {
+			// Sample data for ease of use when running while developing
+			decode = new Decode("samples/input/sample2.txt", "samples/output/sample2.txt");
+		}
+
+		decode.performDecode();
+	}
 }
