ffaa8de01cffb3720195f78e4a139b10dcad2fca
Bob Nisco <BobNisco@gmail.com>
2014-05-08 15:02:18 -0400

    More tests and sample folder cleanup

--- src/huffman/test/HuffmanTest.java
+++ src/huffman/test/HuffmanTest.java
@@ -40,6 +40,39 @@ public class HuffmanTest {
 		HuffmanTest.testEncodeAndDecodeSample("samples/provided-samples/text/sample5.txt", "samples/provided-samples/encoded/sample5_encoded.huf", "samples/provided-samples/encoded/sample5.huf", "samples/provided-samples/encoded/sample5_decoded.txt");
 	}
 
+	@Test
+	public void testEncodeAndDecode01() throws Exception {
+		HuffmanTest.testEncodeAndDecode("samples/input/sample1.txt", "samples/output/sample1_encoded.huf", "samples/output/sample1_decoded.txt");
+	}
+
+	@Test
+	public void testEncodeAndDecode02() throws Exception {
+		HuffmanTest.testEncodeAndDecode("samples/input/sample2.txt", "samples/output/sample2_encoded.huf", "samples/output/sample2_decoded.txt");
+	}
+
+	@Test
+	public void testEncodeAndDecode03() throws Exception {
+		HuffmanTest.testEncodeAndDecode("samples/input/sample3.txt", "samples/output/sample3_encoded.huf", "samples/output/sample3_decoded.txt");
+	}
+
+	@Test
+	public void testEncodeAndDecode04() throws Exception {
+		HuffmanTest.testEncodeAndDecode("samples/input/sample4.txt", "samples/output/sample4_encoded.huf", "samples/output/sample4_decoded.txt");
+	}
+
+	protected static void testEncodeAndDecode(String inputFile, String encodedFile, String decodedFile) throws Exception {
+		String[] args = new String[2];
+		args[0] = inputFile;
+		args[1] = encodedFile;
+		Encode.main(args);
+
+		args[0] = encodedFile;
+		args[1] = decodedFile;
+		Decode.main(args);
+
+		assertEquals(new File(inputFile).length(), new File(decodedFile).length());
+	}
+
 	protected static void testEncodeAndDecodeSample(String inputFile, String encodedFile, String expectedEncodedFile, String decodedFile) throws Exception {
 		// Encode the file and assert
 		EncodeTest.testEncode(inputFile, encodedFile, expectedEncodedFile);
